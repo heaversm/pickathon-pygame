8e8821e124e767ba251be5f5f3efec98602cfd9e
John Brown <thisisjohnbrown@gmail.com>
2018-07-27 02:15:28 +0000

    fix

--- musicbox.py
+++ musicbox.py
@@ -2,22 +2,18 @@ import pygame
 from pygame import *
 import sys
 from gpiozero import Button
-btn = Button(17)
+btn = Button(21)
 
 mixer.pre_init(frequency=22500, size=-16, channels=1, buffer=4096)
 pygame.init()
 pygame.mixer.init()
 screen = pygame.display.set_mode((400,400),0,32)
 
-def hello():
-	print('Hello')
-
-btn.when_pressed = hello
 
 while True:
   for event in pygame.event.get():
     if event.type == QUIT:
-      pygame.quite()
+      pygame.quit()
       sys.exit()
     if event.type == KEYDOWN:
       if event.key==K_ESCAPE:
